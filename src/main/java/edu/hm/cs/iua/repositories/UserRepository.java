0640b4386f9b1fea8572af364d19852c58441732
OnYx9 <schmid34@hm.edu>
2017-11-24 10:39:06 +0100

    Moved JavaScript to own file. java/edu/hm/cs/iua/repositories/UserRepository.java not compiling

--- src/main/java/edu/hm/cs/iua/repositories/UserRepository.java
+++ src/main/java/edu/hm/cs/iua/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package edu.hm.cs.iua.repositories;
 import edu.hm.cs.iua.models.users.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
-}
+/*public interface UserRepository extends CrudRepository<User, Long> {
+}*/
