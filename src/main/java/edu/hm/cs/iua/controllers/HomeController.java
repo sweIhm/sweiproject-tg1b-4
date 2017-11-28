6c5470326a5dbbee9e54abff5ed7785bfec7f8f6
BB Jip <jonathan@geope.de>
2017-11-28 16:06:11 +0100

    Revert "Changes for heroku tests"
    This reverts commit 75d42b330d8221cf764060a1f48b5d0e9b0478ff.

--- src/main/java/edu/hm/cs/iua/controllers/HomeController.java
+++ src/main/java/edu/hm/cs/iua/controllers/HomeController.java
@@ -3,11 +3,10 @@ package edu.hm.cs.iua.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller // so framework can recognize this as a controller class
-@RequestMapping(value = "/", method = RequestMethod.GET)
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping
