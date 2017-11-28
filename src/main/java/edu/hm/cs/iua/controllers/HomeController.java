75d42b330d8221cf764060a1f48b5d0e9b0478ff
OnYx9 <schmid34@hm.edu>
2017-11-28 15:15:01 +0100

    Changes for heroku tests

--- src/main/java/edu/hm/cs/iua/controllers/HomeController.java
+++ src/main/java/edu/hm/cs/iua/controllers/HomeController.java
@@ -3,10 +3,11 @@ package edu.hm.cs.iua.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller // so framework can recognize this as a controller class
-@RequestMapping("/")
+@RequestMapping(value = "/", method = RequestMethod.GET)
 public class HomeController {
 
     @GetMapping
