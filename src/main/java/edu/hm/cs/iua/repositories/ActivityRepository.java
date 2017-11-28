ae0533e59d2278e6b763c2af2feb2f1235bccd0b
BB Jip <jonathan@geope.de>
2017-11-28 16:06:07 +0100

    Revert "Changes for heroku tests"
    This reverts commit a0421d3814a927ad0af0c36ef15bab516d5fae0d.

--- src/main/java/edu/hm/cs/iua/repositories/ActivityRepository.java
+++ src/main/java/edu/hm/cs/iua/repositories/ActivityRepository.java
@@ -2,8 +2,6 @@ package edu.hm.cs.iua.repositories;
 
 import edu.hm.cs.iua.models.Activity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
 }
--- src/main/java/edu/hm/cs/iua/repositories/UserRepository.java
+++ src/main/java/edu/hm/cs/iua/repositories/UserRepository.java
@@ -2,9 +2,7 @@ package edu.hm.cs.iua.repositories;
 
 import edu.hm.cs.iua.models.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 }
