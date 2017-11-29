55fa9bb6a3513cd3b73ab18a8e8845d0cac4f2f3
OnYx9 <schmid34@hm.edu>
2017-11-29 10:59:31 +0100

    Changed sonar cloud recommendations.

--- src/main/java/edu/hm/cs/iua/controllers/ActivityController.java
+++ src/main/java/edu/hm/cs/iua/controllers/ActivityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/activity")
@@ -16,8 +17,8 @@ public class ActivityController {
   
   
   @GetMapping
-  public ArrayList<Activity> listAll() {
-      ArrayList<Activity> activities = new ArrayList<>();
+  public List<Activity> listAll() {
+      List<Activity> activities = new ArrayList<>();
       activityRepository.findAll().forEach(activities::add);
       return activities;
   }
