22e96509ff3ed96f5f87c9127f3fe6dbe0ac57b2
OnYx9 <schmid34@hm.edu>
2017-12-01 14:54:14 +0100

    Added login dialog.

--- src/main/resources/static/controller_new.js
+++ src/main/resources/static/controller_new.js
@@ -6,7 +6,7 @@ app.config(function($mdThemingProvider) {
         .accentPalette('red');
 });
 
-app.controller('IUACtrl', function($scope, $mdSidenav) {
+app.controller('IUACtrl', function($scope, $mdSidenav, $mdDialog) {
 
     // Check if user call's site from http and redirect to https if true.
     /*if (location.protocol !== 'https:')
@@ -31,29 +31,71 @@ app.controller('IUACtrl', function($scope, $mdSidenav) {
         }
     };
 
-    $scope.open_add_activity_dialog = function() {
-
+    $scope.open_add_activity_dialog = function(ev) {
+        $mdDialog.show({
+            controller: addActivityDialogCtrl,
+            templateUrl: 'addActivityDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true
+        })
+            .then(function() {
+                //Load activities
+            });
     };
 
     $scope.open_edit_activity_dialog = function() {
 
     };
 
-    $scope.open_login_dialog = function() {
-
+    $scope.open_login_dialog = function(ev) {
+        $mdDialog.show({
+            controller: loginDialogCtrl,
+            templateUrl: 'loginDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true
+        })
+            .then(function() {
+                //Load activities
+            });
     };
 
-    $scope.open_registration_dialog = function() {
+    $scope.open_registration_dialog = function(ev) {
 
     };
 
     $scope.open_activity_details_dialog = function() {
 
     };
-});
 
-app.controller('AddActivityDialogCtrl', function($scope) {
+    function addActivityDialogCtrl($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+    }
 
+    function loginDialogCtrl($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.login = function(login) {
+            alert($scope.login.email);
+            //Handle login
+        };
+        $scope.forgot_password = function () {
+            //Handle forgot password
+        }
+    }
 });
 
 app.controller('EditActivityDialogCtrl', function($scope) {
