68bbaa53263f1e2a37b5b11b6fe31ff039781087
OnYx9 <schmid34@hm.edu>
2017-12-02 17:50:44 +0100

    Heroku fix.

--- src/main/java/edu/hm/cs/iua/repositories/ActivityRepository.java
+++ src/main/java/edu/hm/cs/iua/repositories/ActivityRepository.java
@@ -2,7 +2,6 @@ package edu.hm.cs.iua.repositories;
 
 import edu.hm.cs.iua.models.Activity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
 }
--- src/main/java/edu/hm/cs/iua/repositories/TokenRepository.java
+++ src/main/java/edu/hm/cs/iua/repositories/TokenRepository.java
@@ -2,7 +2,6 @@ package edu.hm.cs.iua.repositories;
 
 import edu.hm.cs.iua.models.Token;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TokenRepository extends CrudRepository<Token, Long> {
 
