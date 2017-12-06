7dc56e056fc7162211e8d95de6596c06cd1d05d3
OnYx9 <schmid34@hm.edu>
2017-12-06 10:07:16 +0100

    fixed git mistakes.

--- src/main/resources/static/controller.js
+++ src/main/resources/static/controller.js
@@ -1,4 +1,6 @@
-var app = angular.module('IUA_new', ['ngMaterial']);
+var app = angular.module('IUA_new', ['ngMaterial','ngMessages']);
+
+var heroku_address =  'https://iua.herokuapp.com/';
 
 app.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
@@ -11,20 +13,50 @@ function loadActivities ($scope, $http){
         method : 'GET',
         url: (window.location.hostname === 'localhost' ?
             'http://localhost:8080/activity' :
-            'https://iua.herokuapp.com/activity')
+            heroku_address + '/activity')
     }).then(function (response) {
         $scope.activities = response.data;
     });
 }
 
-app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog, $mdToast) {
 
     // Check if user call's site from http and redirect to https if true.
     /*if (location.protocol !== 'https:')
     {
-        alert("This site only works in https:</br>Click okay to get redirected to https:");
+        alert("This site only works in https:. Click ok to get redirected to https:");
         location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
     }*/
+
+    var userid = getCookie("userid");
+    var usertoken = getCookie("usertoken");
+
+    if (userid !== "" && usertoken !== "") {
+        $scope.current_user = {
+            id: userid,
+            token: usertoken
+        };
+        $scope.loginButtonHide = true;
+    } else {
+        $scope.loginButtonHide = false;
+        $scope.current_user = null;
+    }
 
     loadActivities($scope, $http);
 
@@ -33,9 +65,20 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
     };
 
     $scope.search_form_submit = function() {
-        if (document.getElementById('search_input').value !== "") {
-            document.getElementById('search_form').reset();
+        if ($scope.search_text_field.value !== "") {
+            $scope.search_text_field = '';
         }
+    };
+
+    $scope.openUserMenu = function($mdMenu, ev) {
+        $mdMenu.open(ev);
+    };
+
+    $scope.logout = function() {
+        document.cookie = "userid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "usertoken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        $scope.loginButtonHide = false;
+        $scope.current_user = null;
     };
 
     $scope.open_add_activity_dialog = function(ev) {
@@ -73,10 +116,15 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
             parent: angular.element(document.body),
             targetEvent: ev,
             clickOutsideToClose:true
-        })
-            .then(function() {
-                //...
-            });
+        }).then(function(result) {
+            $scope.current_user = result;
+            document.cookie = "userid="+$scope.current_user.id+"; expires=Wed, 6 Dez 2017 12:00:00 UTC; path=/";
+            document.cookie = "usertoken="+$scope.current_user.token+"; expires=Wed, 6 Dez 2017 12:00:00 UTC; path=/";
+        }).finally(function() {
+            if ($scope.current_user !== null) {
+                $scope.loginButtonHide = true;
+            }
+        });
     };
 
     $scope.delete_activity = function(activity) {
@@ -84,7 +132,7 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
             method : 'DELETE',
             url: (window.location.hostname === 'localhost' ?
                 'http://localhost:8080/activity/'+activity.id :
-                'https://iua.herokuapp.com/activity/'+activity.id)
+                heroku_address + '/activity/'+activity.id)
         };
         $http(deleteRequest).then(function() {
             loadActivities($scope, $http);
@@ -101,6 +149,12 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
             .ok('Yes, delete it.')
             .cancel('No, f*** go back.');
         $mdDialog.show(confirm).then(function() {
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent('Activity ' + activity.title + ' deleted.')
+                    .position('bottom right')
+                    .hideDelay(3000)
+            );
             $scope.delete_activity(activity);
         });
     };
@@ -132,6 +186,20 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
         });
     };
 
+    $scope.open_registration_code_dialog = function (user, ev) {
+        var confirm = $mdDialog.prompt()
+            .title('Please enter the code we send to your e-mail:')
+            .placeholder('Confirmation code:')
+            .ariaLabel('Confirmation code:')
+            .targetEvent(ev)
+            .required(true)
+            .ok('Enter')
+            .cancel('Cancel');
+        $mdDialog.show(confirm).then(function(result) {
+            //send code to server and request confirmation
+        });
+    };
+
     function addActivityDialogCtrl($scope, $mdDialog, $http) {
         $scope.cancel = function() {
             $mdDialog.cancel();
@@ -141,7 +209,7 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
                 method : 'POST',
                 url: (window.location.hostname === 'localhost' ?
                     'http://localhost:8080/activity' :
-                    'https://iua.herokuapp.com/activity'),
+                    heroku_address + '/activity'),
                 data: {
                     title: $scope.activity.title,
                     text: $scope.activity.text,
@@ -152,11 +220,17 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
                 $scope.activities = response.data;
             }).then(function () {
                 $scope.cancel();
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Activity ' + $scope.activity.title + ' added.')
+                        .position('bottom right')
+                        .hideDelay(3000)
+                );
             });
         };
     }
 
-    function editActivityDialogCtrl($scope, $mdDialog, $http, activity) {
+    function editActivityDialogCtrl($scope, $mdDialog, $mdToast, $http, activity) {
         $scope.activity = activity;
         $scope.cancel = function() {
             $mdDialog.cancel();
@@ -166,7 +240,7 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
                 method : 'PUT',
                 url: (window.location.hostname === 'localhost' ?
                     'http://localhost:8080/activity/' + activity.id :
-                    'https://iua.herokuapp.com/activity/' + activity.id),
+                    heroku_address + '/activity/' + activity.id),
                 data: {
                     title: $scope.activity.title,
                     text: $scope.activity.text,
@@ -177,6 +251,12 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
                 $scope.activities = response.data;
             }).then(function () {
                 $scope.cancel();
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Activity ' + $scope.activity.title + ' edited.')
+                        .position('bottom right')
+                        .hideDelay(3000)
+                );
             });
         };
     }
@@ -185,38 +265,115 @@ app.controller('IUACtrl', function($scope, $http, $mdSidenav, $mdDialog) {
         $scope.cancel = function() {
             $mdDialog.cancel();
         };
+        $scope.hide = function(value) {
+            $mdDialog.hide(value);
+        };
+        $scope.current_user = null;
         $scope.login = function(login) {
-            //Handle login
+            var getRequest = {
+                method : 'GET',
+                url: (window.location.hostname === 'localhost' ?
+                    'http://localhost:8080/login?email=' + login.email + '&password=' + login.password :
+                    heroku_address + '/login?email=' + login.email + '&password=' + login.password)
+            };
+            $http(getRequest).then(function (response) {
+                $scope.current_user = response.data;
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('You signed in successfully')
+                        .position('bottom right')
+                        .hideDelay(3000)
+                );
+            }).then(function () {
+                if ($scope.current_user !== null) {
+                    $scope.hide($scope.current_user)
+                } else {
+                    $scope.cancel();
+                }
+            }).catch(function (error) {
+                var error_data = error.data;
+                if (error_data.exception.toString() === "edu.hm.cs.iua.exceptions.UserNotValidatedException") {
+                    if (error_data.message !== "") {
+                        $scope.login.timeTillUnlock = error_data.message;
+                        $scope.login_form.login_email.$setValidity('accNotConfirmedLocked', false);
+                    } else {
+                        $scope.login_form.login_email.$setValidity('accNotConfirmedWithResend', false);
+                    }
+                } else {
+                    $scope.login_form.login_email.$setValidity('accNotConfirmedLocked', true);
+                    $scope.login_form.login_email.$setValidity('accNotConfirmedWithResend', true);
+                }
+                if (error_data.exception.toString() === "edu.hm.cs.iua.exceptions.InvalidPasswordException") {
+                    $scope.login_form.login_password.$setValidity('wrongPassword', false);
+                } else {
+                    $scope.login_form.login_password.$setValidity('wrongPassword', true);
+                }
+                if (error_data.exception.toString() === "edu.hm.cs.iua.exceptions.UserNotFoundException") {
+                    $scope.login_form.login_email.$setValidity('emailNotExists', false);
+                } else {
+                    $scope.login_form.login_email.$setValidity('emailNotExists', true);
+                }
+            });
         };
         $scope.forgot_password = function () {
             //Handle forgot password
         }
     }
 
-    function registrationDialogCtrl($scope, $mdDialog) {
+    function registrationDialogCtrl($scope, $mdDialog, $mdToast) {
         $scope.cancel = function() {
             $mdDialog.cancel();
         };
+
         $scope.registration = function(reg) {
             if (reg.password !== reg.passwordConfirm) {
-                $scope.reg_form.reg_password_confirm.$setValidity("password", false);
+                $scope.reg_form.reg_password_confirm.$setValidity('password', false);
                 return;
+            } else {
+                $scope.reg_form.reg_password_confirm.$setValidity('password', true);
             }
             var postRequest = {
                 method : 'POST',
                 url: (window.location.hostname === 'localhost' ?
-                    'http://localhost:8080/registration' :
-                    'https://iua.herokuapp.com/registration'),
+                    'http://localhost:8080/register' :
+                    heroku_address + '/register'),
                 data: {
-                    username: $scope.reg.name,
+                    name: $scope.reg.name,
                     email: $scope.reg.email,
                     password: $scope.reg.password
                 }
             };
             $http(postRequest).then(function (response) {
-                // Work with response
+                //...
             }).then(function () {
                 $scope.cancel();
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('User ' + $scope.reg.name + ' created.')
+                        .position('bottom right')
+                        .hideDelay(3000)
+                );
+            }).catch(function (error) {
+                var error_data = error.data;
+                if (error_data.exception.toString() === "edu.hm.cs.iua.exceptions.InvalidDataException") {
+                    if (error_data.message === "Email invalid.") {
+                        $scope.reg_form.reg_email.$setValidity('noHMorCalEmail', false);
+                    } else {
+                        $scope.reg_form.reg_email.$setValidity('noHMorCalEmail', true);
+                    }
+                } else {
+                    $scope.reg_form.reg_email.$setValidity('noHMorCalEmail', true);
+                }
+                if (error_data.exception.toString() === "edu.hm.cs.iua.exceptions.EmailAlreadyTakenException") {
+                    $scope.reg_form.reg_email.$setValidity('emailAlreadyTaken', false);
+                } else {
+                    $scope.reg_form.reg_email.$setValidity('emailAlreadyTaken', true);
+                }
+                if (error_data.exception.toString() === "edu.hm.cs.iua.exceptions.UsernameAlreadyTakenException") {
+                    $scope.reg_form.reg_name.$setValidity('nameAlreadyTaken', false);
+                } else {
+                    $scope.reg_form.reg_name.$setValidity('nameAlreadyTaken', true);
+                }
             });
         };
     }
