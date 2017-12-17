58dd4c8e08ff53522caa985e325a2808f616c43b
BB Jip <jonathan@geope.de>
2017-12-17 20:02:49 +0100

    Fixed SonarCloud code smells

--- src/main/java/edu/hm/cs/iua/controllers/RegistrationController.java
+++ src/main/java/edu/hm/cs/iua/controllers/RegistrationController.java
@@ -22,11 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.mail.MessagingException;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
